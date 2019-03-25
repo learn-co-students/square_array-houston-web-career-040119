0a651740018372b30665ad82be0bef673facb26d
Nathan Richardson <nathan.richardson94@gmail.com>
2019-03-25 14:46:42 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,22 @@
+
 def square_array(array)
   # your code here
+  squared_array = []
+  array.each do |array|
+    squared_array << array ** 2
+  end
+  squared_array
 end
+
+
+=begin
+def square_array(array)
+  squared_array = []
+  array.each do |array|
+    squared_array << array.collect {|x| x ** 2 }
+  end
+  squared_array
+end 
+numbers = [2, 4, 6]
+puts square_array(numbers)
+=end
